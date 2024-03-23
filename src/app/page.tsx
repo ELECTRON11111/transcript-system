1bccc071c30f5dc584760ad31ba4b46ba767a946
Omoniyi Opemipo [Electron] <omoniyiopempo6@gmail.com>
2024-03-23 14:32:09 +0100

    [Bug fixes] strokeWidth, StrokeLineCap and strokeLinejoin in svg files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,8 +37,8 @@ export default function Home(): any {
           </svg>
 
 
-          <svg xmlns="http://www.w3.org/2000/svg" id="Burger-icon" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" onClick={(e) => burgerClickedHandler(e)} className="w-6 h-6 cursor-pointer sm:hidden">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+          <svg xmlns="http://www.w3.org/2000/svg" id="Burger-icon" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" onClick={(e) => burgerClickedHandler(e)} className="w-6 h-6 cursor-pointer sm:hidden">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
           </svg>
         </div>
 
