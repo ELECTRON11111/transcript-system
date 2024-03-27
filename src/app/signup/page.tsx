87bcf9ca5cd6fecb7f45d453df704b549c16acbd
Omoniyi Opemipo [Electron] <omoniyiopempo6@gmail.com>
2024-03-28 00:41:21 +0100

    Worked on the form validation of the sign up inputs

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,8 +4,49 @@ import React from "react";
 import Link from "next/link";
 
 export default function signup():any {
-    // const [termsAgreed, changeTermsAgreed] = useState(false);
-    
+    const [formData, updateFormData] = useState({
+        email: "",
+        password: "",
+        university: "",
+        role: "",
+        areTermsAgreed: false
+    });
+    const [errorState, updateErrorState] = useState(true);
+    const [emailErrorState, updateEmailErrorState] = useState(true);
+    const [passwordErrorState, updatePasswordErrorState] = useState(true);
+
+    const submitHandler = (e:any) => {
+        e.preventDefault();
+        console.log("Form Submitted");
+    }
+
+    const handleEmailError = (e: any) => {
+        if (e.target.value === "") {
+            updateEmailErrorState(true);
+        } else {
+            updateEmailErrorState(false);
+        }
+    }
+
+    const handleConfirmPassword = (e: any) => {
+        if (e.target.value !== formData.password) {
+            updatePasswordErrorState(true);
+        } else {
+            updatePasswordErrorState(false);
+        }
+    }
+
+    const handleChange = (event: any) => {
+        event.persist();
+        const { name, value, type, checked } = event.target;
+
+        // Take previous state and update only the input field changed.
+        updateFormData(prevState => ({
+            ...prevState,
+            [name]: type === "checkbox"? checked: value
+        }));
+    }
+     
     return (
         <div id="login" className="w-full bg-white p-4 py-8 md:flex md:flex-col md:justify-center md:px-60 md:pb-16">
             <div id="head" className="my-8">
@@ -15,21 +56,29 @@ export default function signup():any {
                 </h1>
             </div>
             
-            <form action="#" className="flex flex-col justify-around" onSubmit={(e) => e.preventDefault()}>
-                <input type="email" className="input" placeholder={`Enter University email`} />
-                <input type="password" className="input" placeholder="Enter Password" />
-                <input type="password" className="input" placeholder="Confirm Password" />
-                <select name="uni_affiliation" id="select_uni" className="input text-gray-400">
+            <form action="#" className="flex flex-col justify-around" onSubmit={(e) => submitHandler(e)}>
+                <input type="email" name="email" className="input" onChange = {(e:any) => {handleChange(e); handleEmailError(e)}} placeholder={`Enter University email`} />
+                <input type="password" name="password" onChange = {(e) => {handleChange(e); handleConfirmPassword(e)}} className="input" placeholder="Enter Password" />
+                <input type="password" className="input" onChange = {(e) => handleConfirmPassword(e)} placeholder="Confirm Password" />
+
+                {
+                    passwordErrorState ? <p className="text-red-700 text-sm">Passwords do not match.</p>
+                                : <p className="text-green-700 text-sm">Passwords match, continue ..</p>
+                } 
+
+                <select name="university" defaultValue={"bellsTech"} onChange = {(e) => handleChange(e)} id="select_uni" className="input text-gray-400">
                     <option value="bellsTech">Bells University of Technology</option>
                     <option value="Redeemers">Redeemers University</option>
                     <option value="Covenant">Covenant University</option>
                 </select>
-                <input type="text" name="role" id="role" className="input" placeholder="What is your role in the university?"/>
+
+                <input type="text" name="role" id="role" onChange = {(e) => handleChange(e)} className="input" placeholder="What is your role in the university?"/>
                 <div id="terms_and_services" className="flex gap-2 items-baseline">
-                    <input type="checkbox" name="terms_and_conditions" id="" />
-                    <span> I agree to the <Link href={"#"}>terms and Conditions.</Link></span>
+                    <input type="checkbox" onChange={(e) => {handleChange(e)}} name="areTermsAgreed" />
+                    <span> I agree to the <Link href={"#"} className="font-light underline">terms and Conditions.</Link></span>
                 </div>
-                <button type="submit" className="my-4 p-2 w-full bg-purple-600 rounded text-white transition duration-300 ease-out hover:shadow-lg">Submit</button>
+                
+                <button type="submit" className={`${formData.areTermsAgreed? "opacity-100": "opacity-60"} my-4 p-2 w-full bg-purple-600 rounded text-white transition duration-300 ease-out hover:shadow-lg`} disabled={!formData.areTermsAgreed}>Submit</button>
             </form>
 
             <p className="my-2 text-md">Already have an account? <Link href={"/#login"} className="font-light underline">   Log In</Link></p>
